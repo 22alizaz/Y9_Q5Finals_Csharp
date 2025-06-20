a311c3018eeffec790d10c7a3b5a8c0c7b15dc9f
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2025-06-20 16:11:28 +0100

    Add project files.

---
+++ Y9_Q5Finals_Csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Channels;
+
+namespace Y9_Q5Finals_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int e1 = 50;
+            int e2 = 50;
+            int e3 = 50;
+
+
+            Console.WriteLine("Input the number of essays handed in late");
+            int late = Convert.ToInt32(Console.ReadLine());
+            float total = e1 + e2 + e3;
+            while (total >= 0)
+            {
+                if (late==1)
+                {
+                        total = total - 10;
+                    
+                } 
+                if (late>=2) 
+                {
+                    total = total / 2;
+                }
+            }
+            if (total<=-1)
+            {
+                float leftover = total*-1;
+                total = total + leftover;   
+            }
+            Console.WriteLine("The total mark is " + total);
+
+        }
+    }
+}
